7b63ea957b373ee4e415037cba51dcfd1a0cd88f
Timothy <aytimothy@aytimothy.xyz>
2019-11-25 00:39:39 +1000

    Refactored names to proper names, added Json.NET and Colorful.Console

--- Test Client/Properties/Resources.Designer.cs
+++ Test Client/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace aytimothy.EIChatbot.Client.Properties {
+namespace aytimothy.EIChatbot.Debugger.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace aytimothy.EIChatbot.Client.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("aytimothy.EIChatbot.Client.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("aytimothy.EIChatbot.Debugger.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
