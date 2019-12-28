fb3a75fa4eaa1c9fe6b432d7c1afd79a7cb35a52
Timothy <aytimothy@aytimothy.xyz>
2019-12-28 21:39:11 +0800

    Debugger UI Draft

---
+++ Test Client/TopWindow.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Test_Client
+{
+    partial class TopWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.MainWindowMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenAgentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveOutputToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.QuitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.OutputDataGridView = new System.Windows.Forms.DataGridView();
+            this.Timestamp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Message = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.DetailDataGridView = new System.Windows.Forms.DataGridView();
+            this.Parameter = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.MainWindowMenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DetailDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainWindowMenuStrip
+            // 
+            this.MainWindowMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem});
+            this.MainWindowMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MainWindowMenuStrip.Name = "MainWindowMenuStrip";
+            this.MainWindowMenuStrip.Size = new System.Drawing.Size(765, 24);
+            this.MainWindowMenuStrip.TabIndex = 0;
+            this.MainWindowMenuStrip.Text = "menuStrip1";
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenAgentToolStripMenuItem,
+            this.SaveOutputToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.QuitToolStripMenuItem});
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.FileToolStripMenuItem.Text = "File";
+            // 
+            // OpenAgentToolStripMenuItem
+            // 
+            this.OpenAgentToolStripMenuItem.Name = "OpenAgentToolStripMenuItem";
+            this.OpenAgentToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.OpenAgentToolStripMenuItem.Text = "Open Agent";
+            // 
+            // SaveOutputToolStripMenuItem
+            // 
+            this.SaveOutputToolStripMenuItem.Name = "SaveOutputToolStripMenuItem";
+            this.SaveOutputToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.SaveOutputToolStripMenuItem.Text = "Save Output";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // QuitToolStripMenuItem
+            // 
+            this.QuitToolStripMenuItem.Name = "QuitToolStripMenuItem";
+            this.QuitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.QuitToolStripMenuItem.Text = "Quit";
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(12, 414);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(660, 20);
+            this.InputTextBox.TabIndex = 1;
+            this.InputTextBox.TextChanged += new System.EventHandler(this.InputTextBox_TextChanged);
+            // 
+            // OutputDataGridView
+            // 
+            this.OutputDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OutputDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Timestamp,
+            this.Type,
+            this.Message});
+            this.OutputDataGridView.Location = new System.Drawing.Point(12, 27);
+            this.OutputDataGridView.Name = "OutputDataGridView";
+            this.OutputDataGridView.Size = new System.Drawing.Size(398, 381);
+            this.OutputDataGridView.TabIndex = 2;
+            this.OutputDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.OutputDataGridView_CellContentClick);
+            // 
+            // Timestamp
+            // 
+            this.Timestamp.HeaderText = "Timestamp";
+            this.Timestamp.Name = "Timestamp";
+            this.Timestamp.ReadOnly = true;
+            // 
+            // Type
+            // 
+            this.Type.HeaderText = "Type";
+            this.Type.Name = "Type";
+            this.Type.ReadOnly = true;
+            // 
+            // Message
+            // 
+            this.Message.HeaderText = "Message";
+            this.Message.Name = "Message";
+            this.Message.ReadOnly = true;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(678, 412);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.TabIndex = 3;
+            this.SubmitButton.Text = "button1";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            // 
+            // DetailDataGridView
+            // 
+            this.DetailDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DetailDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Parameter,
+            this.Value});
+            this.DetailDataGridView.Location = new System.Drawing.Point(416, 27);
+            this.DetailDataGridView.Name = "DetailDataGridView";
+            this.DetailDataGridView.Size = new System.Drawing.Size(337, 381);
+            this.DetailDataGridView.TabIndex = 4;
+            // 
+            // Parameter
+            // 
+            this.Parameter.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Parameter.HeaderText = "";
+            this.Parameter.Name = "Parameter";
+            this.Parameter.ReadOnly = true;
+            this.Parameter.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // Value
+            // 
+            this.Value.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Value.HeaderText = "";
+            this.Value.Name = "Value";
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "openFileDialog1";
+            // 
+            // TopWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 442);
+            this.Controls.Add(this.DetailDataGridView);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.OutputDataGridView);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.MainWindowMenuStrip);
+            this.MainMenuStrip = this.MainWindowMenuStrip;
+            this.Name = "TopWindow";
+            this.Text = "EIChatbot Debugger";
+            this.MainWindowMenuStrip.ResumeLayout(false);
+            this.MainWindowMenuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DetailDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MainWindowMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.DataGridView OutputDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Timestamp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Message;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.ToolStripMenuItem OpenAgentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveOutputToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem QuitToolStripMenuItem;
+        private System.Windows.Forms.DataGridView DetailDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Parameter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.SaveFileDialog SaveFileDialog;
+    }
+}
+
---
+++ Test Client/TopWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test_Client
+{
+    public partial class TopWindow : Form
+    {
+        public TopWindow() {
+            InitializeComponent();
+        }
+
+        private void OutputDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+
+        }
+
+        private void InputTextBox_TextChanged(object sender, EventArgs e) {
+
+        }
+    }
+}
